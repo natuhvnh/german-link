a75b35dc8b7773cda2a8ea93e3a167175c76da88
natuhvnh <natuhvnh@gmail.com>
2019-11-29 10:10:55 +0700

    Setup page and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import Brand from "./components/Brand";
+import TopBar from "./components/TopBar";
+import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Brand />
+      <TopBar />
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="login" component={Login} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Brand.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Brand extends Component {
+  render() {
+    return <div>Brand</div>;
+  }
+}
+
+export default Brand;
