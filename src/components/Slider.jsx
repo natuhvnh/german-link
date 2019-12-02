023a0ff2c36619e61ee78c2f8b78a4b4f38b70d9
natuhvnh <natuhvnh@gmail.com>
2019-12-03 00:02:55 +0700

    Edit slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -10,57 +10,59 @@ class Slider extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="slides" className="carousel" data-ride="carousel">
-          <ul className="carousel-indicators">
-            {this.state.images.map((image, index) => {
-              return index === 0 ? (
-                <li
-                  data-target="#slides"
-                  data-slide-to={index.toString()}
-                  className="active"
-                  key={index}></li>
-              ) : (
-                <li data-target="#slides" data-slide-to={index.toString()} key={index}></li>
-              );
-            })}
-          </ul>
-          <div className="carousel-inner">
-            {this.state.images.map((image, index) => {
-              return index === 0 ? (
-                <div className="carousel-item active" key={index}>
-                  <img src={image} alt="slide" />
-                  <div className="carousel-caption">
-                    <button className="btn btn-outline-warning" onClick={this.handleClose}>
-                      <i className="fa fa-plus"></i>
-                    </button>
-                    <button className="btn btn-outline-warning" onClick={this.handleClose}>
-                      <i className="fa fa-pencil"></i>
-                    </button>
-                    <button className="btn btn-outline-warning" onClick={this.handleClose}>
-                      <i className="fa fa-trash"></i>
-                    </button>
+        <div className="container-fluid">
+          <div id="slides" className="carousel slide" data-ride="carousel">
+            <ul className="carousel-indicators">
+              {this.state.images.map((image, index) => {
+                return index === 0 ? (
+                  <li
+                    data-target="#slides"
+                    data-slide-to={index.toString()}
+                    className="active"
+                    key={index}></li>
+                ) : (
+                  <li data-target="#slides" data-slide-to={index.toString()} key={index}></li>
+                );
+              })}
+            </ul>
+            <div className="carousel-inner">
+              {this.state.images.map((image, index) => {
+                return index === 0 ? (
+                  <div className="carousel-item active" key={index}>
+                    <img src={image} alt="slide" className="img-fluid mx-auto d-block" />
+                    <div className="carousel-caption">
+                      <button className="btn btn-outline-warning" onClick={this.handleClose}>
+                        <i className="fa fa-plus"></i>
+                      </button>
+                      <button className="btn btn-outline-warning" onClick={this.handleClose}>
+                        <i className="fa fa-pencil"></i>
+                      </button>
+                      <button className="btn btn-outline-warning" onClick={this.handleClose}>
+                        <i className="fa fa-trash"></i>
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ) : (
-                <div className="carousel-item" key={index}>
-                  <img src={image} alt="slide" />
-                  <div className="carousel-caption">
-                    <button className="btn btn-outline-warning" onClick={this.handleClose}>
-                      <i className="fa fa-plus"></i>
-                    </button>
-                    <button className="btn btn-outline-warning" onClick={this.handleClose}>
-                      <i className="fa fa-pencil"></i>
-                    </button>
-                    <button className="btn btn-outline-warning" onClick={this.handleClose}>
-                      <i className="fa fa-trash"></i>
-                    </button>
+                ) : (
+                  <div className="carousel-item" key={index}>
+                    <img src={image} alt="slide" className="img-fluid mx-auto d-block" />
+                    <div className="carousel-caption">
+                      <button className="btn btn-outline-warning" onClick={this.handleClose}>
+                        <i className="fa fa-plus"></i>
+                      </button>
+                      <button className="btn btn-outline-warning" onClick={this.handleClose}>
+                        <i className="fa fa-pencil"></i>
+                      </button>
+                      <button className="btn btn-outline-warning" onClick={this.handleClose}>
+                        <i className="fa fa-trash"></i>
+                      </button>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
+          <hr />
         </div>
-        <hr />
       </React.Fragment>
     );
   }
